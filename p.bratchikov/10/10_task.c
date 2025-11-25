4e3c4e5e6afb4c42603eae35cad8521484c005f7
Pavel-Bratchikov <p.bratchikov@g.nsu.ru>
2025-11-25 11:53:14 +0700

    some fix

--- p.bratchikov/10/10_task.c
+++ p.bratchikov/10/10_task.c
@@ -1,23 +1,65 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-    if (argc == 1) { return 1; }
+int main(int argc, char *argv[]) 
+{
+
+    if (argc > 1 && (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0)) 
+    {
+        printf("\nThis program forks a child process to execute a command passed as arguments.\n");
+        printf("\nUsage: %s <command> [args...]\n", argv[0]);
+        return 0;
+    }
+
+    if (argc == 1) 
+    {
+        fprintf(stderr, "\nError: No command provided. Use --help for usage.\n");
+        return 1;
+    }
 
     pid_t pid = fork();
-    if (pid == -1) { return 1; }
 
-    if (pid == 0) {
-        execvp(argv[1], argv + 1);
+    if (pid == -1) 
+    {
+        fprintf(stderr, "\nfork() error: %s\n", strerror(errno));
         return 1;
-    } else {
-        int stat_loc;
-        wait(&stat_loc);
+    }
 
-        if (WIFEXITED(stat_loc)) {
-            printf("\nChild process (pid: %d) finished with exit code %d\n",
-                   pid, WEXITSTATUS(stat_loc));
+    if (pid == 0) 
+    {
+        execvp(argv[1], argv + 1);
+        fprintf(stderr, "\nexecvp() error: %s\n", strerror(errno));
+        return 1;
+    }
+
+    else 
+    {
+        
+        int stat_loc;
+
+        if (wait(&stat_loc) == -1) 
+        {
+            fprintf(stderr, "\nwait() error: %s\n", strerror(errno));
+            return 1;
+        }
+
+        if (WIFEXITED(stat_loc)) 
+        {
+            printf("\nChild process (pid: %d) finished with exit code %d\n", pid, WEXITSTATUS(stat_loc));
+        }
+
+        else if (WIFSIGNALED(stat_loc)) 
+        {
+            printf("\nChild process (pid: %d) was terminated by signal %d\n", pid, WTERMSIG(stat_loc));
+        }
+
+        else 
+        {
+            printf("\nChild process (pid: %d) finished in unknown state\n", pid);
         }
     }
 
--- p.bratchikov/9/9_task.c
+++ p.bratchikov/9/9_task.c
@@ -1,19 +1,51 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <stdio.h>
+#include <errno.h>
+#include <string.h>
 
-int main() {
+int main(int argc, char *argv[]) {
+    if (argc > 1 && (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0)) {
+        printf("This program forks and executes: cat blur_faces.py\n");
+        printf("Usage: %s [--help|-h]\n", argv[0]);
+        return 0;
+    }
+
     pid_t pid = fork();
-    if (pid == -1) { return 1; }
+
+    if (pid == -1) {
+        fprintf(stderr, "fork() error: %s\n", strerror(errno));
+        return 1;
+    }
 
     if (pid == 0) {
         execlp("cat", "cat", "blur_faces.py", NULL);
+
+        fprintf(stderr, "execlp() error: %s\n", strerror(errno));
         return 1;
-    } else {
-        // Parent process
-        if(wait(NULL) != -1) {
-            printf("\nChild process (pid: %d) finished\n", pid);
+    }
+
+    else {
+        int status;
+        if (wait(&status) == -1) {
+            fprintf(stderr, "wait() error: %s\n", strerror(errno));
+            return 1;
         }
+
+        if (WIFEXITED(status)) 
+        {
+            printf("\nChild process (pid: %d) finished with exit code %d\n", pid, WEXITSTATUS(status));
+        }
+
+        else if (WIFSIGNALED(status)) 
+        {
+            printf("\nChild process (pid: %d) was terminated by signal %d\n", pid, WTERMSIG(status));
+        }
+
+        else 
+            {
+                printf("\nChild process (pid: %d) finished in unknown state\n", pid);
+            }
     }
 
     return 0;
