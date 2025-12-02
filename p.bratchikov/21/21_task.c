78ec0ed1be5d37e6f2cc9d80896f4de9e3c0b926
Pavel-Bratchikov <p.bratchikov@g.nsu.ru>
2025-12-02 11:00:56 +0700

    some fix

--- p.bratchikov/21/21_task.c
+++ p.bratchikov/21/21_task.c
@@ -1,23 +1,30 @@
+#define _POSIX_C_SOURCE 200809L
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int count = 0;
 
-void handleSIGINT() {
+void handleSIGINT(int sig) {
     printf("\a");
     fflush(NULL);
     count++;
 }
 
-void handleSIGQUIT() {
-    printf("\nThe signal sounded %d times.", count);
+void handleSIGQUIT(int sig) {
+    printf("\nThe signal sounded %d times.\n", count);
     exit(0);
 }
 
 int main() {
-    signal(SIGINT, &handleSIGINT);
-    signal(SIGQUIT, &handleSIGQUIT);
+    struct sigaction sa_int = {0};
+    sa_int.sa_handler = handleSIGINT;
+    sigaction(SIGINT, &sa_int, NULL);
 
-    while (1);
+    struct sigaction sa_quit = {0};
+    sa_quit.sa_handler = handleSIGQUIT;
+    sigaction(SIGQUIT, &sa_quit, NULL);
+
+    while (1) pause();
 }
