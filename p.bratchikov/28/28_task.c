4fb9a4d46b7421bfeeafceffe06d472f8f573601
Pavel-Bratchikov <p.bratchikov@g.nsu.ru>
2025-12-09 06:57:46 +0700

    some fix

--- p.bratchikov/28/28_task.c
+++ p.bratchikov/28/28_task.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <libgen.h>
 #include <unistd.h>
+#include <string.h>
 #include <time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 void print_help(void) {
     printf("Usage: program\n");
     printf("Generates 100 random numbers (0-99), sorts them, and prints 10 numbers per line.\n");
-    printf("Sort is performed via 'sort -n' using p2open/p2close.\n");
+    printf("Sort is performed via 'sort -n' using pipes and fork.\n");
     printf("Options:\n");
     printf("  -h, --help   Display this help message\n");
 }
@@ -25,29 +27,61 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    FILE *fp[2];
+    int pipe_to_sort[2];
+    int pipe_from_sort[2];   
 
-    if (p2open("sort -n", fp) == -1) { return 1; }
+    if (pipe(pipe_to_sort) == -1 || pipe(pipe_from_sort) == -1) {
+        perror("pipe");
+        return EXIT_FAILURE;
+    }
+
+    pid_t pid = fork();
+    if (pid < 0) {
+        perror("fork");
+        return EXIT_FAILURE;
+    }
+
+    if (pid == 0) {
+        dup2(pipe_to_sort[0], STDIN_FILENO);
+
+        
+        dup2(pipe_from_sort[1], STDOUT_FILENO);
+
+
+        close(pipe_to_sort[1]);
+        close(pipe_from_sort[0]);
+
+
+        execlp("sort", "sort", "-n", NULL);
+
+        perror("execlp");
+        exit(EXIT_FAILURE);
+    }
+
+
+    close(pipe_to_sort[0]);
+    close(pipe_from_sort[1]);
 
     srand(time(NULL));
     for (int i = 0; i < 100; i++) {
-        fprintf(fp[0], "%d\n", rand() % 100);
+        dprintf(pipe_to_sort[1], "%d\n", rand() % 100);
     }
-
-    fclose(fp[0]);
+    close(pipe_to_sort[1]);
 
     char c;
     int count = 0;
-    while (read(fileno(fp[1]), &c, 1) == 1) {
+
+    while (read(pipe_from_sort[0], &c, 1) == 1) {
         if (c == '\n') {
             printf(++count % 10 == 0 ? "\n" : " ");
             continue;
         }
-
-        printf("%c", c);
+        putchar(c);
     }
 
-    p2close(fp);
+    close(pipe_from_sort[0]);
 
-    return 0;
+    wait(NULL);
+
+    return EXIT_SUCCESS;
 }
