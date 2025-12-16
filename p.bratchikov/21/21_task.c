561951b5f10090e595dbbc8fd17daa551ab757a4
Pavel-Bratchikov <p.bratchikov@g.nsu.ru>
2025-12-16 11:22:01 +0700

    fix

--- p.bratchikov/21/21_task.c
+++ p.bratchikov/21/21_task.c
@@ -1,72 +1,40 @@
-#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <signal.h>
 #include <unistd.h>
 
-static volatile sig_atomic_t count = 0;
+int beep_count = 0;
 
-void handle_sigint(int sig) {
-    (void)sig;
-    write(STDOUT_FILENO, "\a", 1);
-    count++;
+void sigint_handler(int sig)
+{
+    signal(SIGINT, sigint_handler);
+    beep_count++;
+    printf("\r\aBEEP! \n");
+    fflush(stdout);
 }
 
-void handle_sigquit(int sig) {
-    (void)sig;
-
-    char buf[128];
-    int len = snprintf(buf, sizeof(buf),
-                       "\nThe signal sounded %d times.\n",
-                       (int)count);
-
-    write(STDOUT_FILENO, buf, len);
-
-    _exit(0);  
+void sigquit_handler(int sig)
+{
+    printf("\r\e[2K");
+    printf("\nProgram finished.\n");
+    printf("Count of signals: %d\n", beep_count);
+    exit(0);
 }
 
-int main(int argc, char **argv) {
+int main()
+{
+    signal(SIGINT, sigint_handler);
+    signal(SIGQUIT, sigquit_handler);
 
-    if (argc == 2) {
-        if (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0) {
-            printf("This program installs handlers for SIGINT and SIGQUIT.\n");
-            printf("Press Ctrl+C to produce a beep and increment the counter.\n");
-            printf("Press Ctrl+\\ to print the number of beeps and exit.\n");
-            printf("Usage: %s [--help | -h]\n", argv[0]);
-            return EXIT_SUCCESS;
-        } else {
-            fprintf(stderr, "Error: unknown option '%s'\n", argv[1]);
-            return EXIT_FAILURE;
-        }
-    } else if (argc > 2) {
-        fprintf(stderr, "Error: too many arguments\n");
-        return EXIT_FAILURE;
-    }
+    printf("Program started. PID: %d\n", getpid());
+    printf("Press Ctrl+C for sound signal\n");
+    printf("Press Ctrl+\\ to show statistics and exit\n");
+    printf("========================================\n");
 
-    struct sigaction sa;
-
-
-    memset(&sa, 0, sizeof(sa));
-    sa.sa_handler = handle_sigint;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = SA_RESTART;          
-    if (sigaction(SIGINT, &sa, NULL) == -1) {
-        perror("sigaction SIGINT");
-        return EXIT_FAILURE;
-    }
-
-    memset(&sa, 0, sizeof(sa));
-    sa.sa_handler = handle_sigquit;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = SA_RESTART;
-    if (sigaction(SIGQUIT, &sa, NULL) == -1) {
-        perror("sigaction SIGQUIT");
-        return EXIT_FAILURE;
-    }
-
-    for (;;) {
+    while (1)
+    {
         pause();
     }
 
-    return EXIT_SUCCESS;
+    return 0;
 }
